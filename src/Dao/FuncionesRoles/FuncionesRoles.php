cfef44039f70c7b9ba19112b89953e548cfc2a6a
imaginuz <zunig4business@gmail.com>
2024-12-02 21:16:21 -0600

    Refactor getFuncionesRoles method and add agregarFuncionesRoles

--- src/Dao/FuncionesRoles/FuncionesRoles.php
+++ src/Dao/FuncionesRoles/FuncionesRoles.php
@@ -6,10 +6,10 @@ use Dao\Table;
 
 class FuncionesRoles extends Table
 {
-    public static function getFuncionesRoles($filter = '')
+    public static function getFuncionesRoles()
     {
-        $sqlstr = "SELECT * FROM funciones_roles WHERE fncod like :filter;";
-        $params = array('filter' => '%' . $filter . '%');
+        $sqlstr = "SELECT * FROM funciones_roles";
+        $params = [];
         $registros = self::obtenerRegistros($sqlstr, $params);
         return $registros;
     }
@@ -21,4 +21,22 @@ class FuncionesRoles extends Table
         $registros = self::obtenerUnRegistro($sqlstr, $params);
         return $registros;
     }
+
+    public static function agregarFuncionesRoles($rolescod, $fncod, $fnrolest, $fnexp){
+	
+        $sqlstr = "INSERT INTO funciones_roles (
+            rolescod, 
+            fncod, 
+            fnrolest, 
+            fnexp) 
+            VALUES (
+            :rolescod, 
+            :fncod, 
+            :fnrolest, 
+            :fnexp)";
+        $params = ['rolescod' => $rolescod, 'fncod' => $fncod, 'fnrolest' => $fnrolest, 'fnexp' => $fnexp];
+        $registros = self::executeNonQuery($sqlstr, $params);
+        return $registros;
+    
+        }
 }

f6590dbea391d0d4894322f371c733ea47efab59
imaginuz <zunig4business@gmail.com>
2024-12-02 21:02:25 -0600

    Add roles by filter and ID in FuncionesRoles class

--- src/Dao/FuncionesRoles/FuncionesRoles.php
+++ src/Dao/FuncionesRoles/FuncionesRoles.php
@@ -6,5 +6,19 @@ use Dao\Table;
 
 class FuncionesRoles extends Table
 {
-    
+    public static function getFuncionesRoles($filter = '')
+    {
+        $sqlstr = "SELECT * FROM funciones_roles WHERE fncod like :filter;";
+        $params = array('filter' => '%' . $filter . '%');
+        $registros = self::obtenerRegistros($sqlstr, $params);
+        return $registros;
+    }
+        
+    public static function obtenerPorId($fncod)
+    {
+        $sqlstr = "SELECT * FROM funciones_roles WHERE fncod = :fncod";
+        $params = ['fncod' => $fncod];
+        $registros = self::obtenerUnRegistro($sqlstr, $params);
+        return $registros;
+    }
 }
